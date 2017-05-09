7df9bf1b45b1e6e97b64f7f39467de87d159d8f7
Hannah <hannah.munoz92@gmail.com>
2017-05-09 16:55:23 -0700

    tensorflow added

--- PIN-keystroke/app/src/main/java/com/example/hannah/pin_keystroke/Testing.java
+++ PIN-keystroke/app/src/main/java/com/example/hannah/pin_keystroke/Testing.java
@@ -2,17 +2,30 @@ package com.example.hannah.pin_keystroke;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import org.tensorflow.contrib.android.TensorFlowInferenceInterface;
 
 /**
  * Created by hannah on 5/6/17.
  */
 
 public class Testing extends AppCompatActivity {
+
+    static {
+        System.loadLibrary("tensorflow_inference");
+    }
+
+
+    private TensorFlowInferenceInterface inferenceInterface;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         //create the layout
         setContentView(R.layout.testing);
+
+        //inferenceInterface = new TensorFlowInferenceInterface();
+
     }
+
 }
